40bfa09ad798ed420710302b9d74c85fb3ca6939
p1tcode <perivar@gmail.com>
2019-10-27 00:35:14 +0200

    Fixed more bugs with the options screen. Think everything is working as expected now. Even after reloading ui.
    Fixed so the correct values are set when first time loading the addon.
    
    
    Signed-off-by: p1tcode <perivar@gmail.com>

--- Options.lua
+++ Options.lua
@@ -1,6 +1,8 @@
 local config = LibStub("AceConfig-3.0")
 local dialog = LibStub("AceConfigDialog-3.0")
 
+local CHATCHANNELS = { ["RAID"] = "RAID", ["PARTY"] = "PARTY", ["YELL"] = "YELL", ["SAY"] = "SAY", ["NONE"] = "NONE"}
+
 function SAC:CreateOptions()
 	
 	SAC.Options = {
@@ -8,27 +10,41 @@ function SAC:CreateOptions()
 		handler = SAC,
 		type = 'group',
 		args = {
-
-		},
-	}
-
-	SAC.Options_Auras = {
-		name = "Auras",
-		handler = SAC,
-		type = 'group',
-		args = {
-			description = {
+			header = {
 				order = 0,
+				type = 'header',
+				name = "General"
+			},
+			channel = {
+				order = 1,
+				name = "Channel",
+				type = 'select',
+				values = CHATCHANNELS,
+				set = 'SetChatChannel',
+				get = 'GetChatChannel',
+			},
+			recursiveEnable = {
+				order = 2,
+				name = "Recursive Channel",
+				desc = "Recursively announce to the next lower channel if your not in your selected Chat channel. Raid -> Party -> Yell",
+				type = 'toggle',
+				set = 'Set',
+				get = 'Get',
+			},
+			
+			-- Aura section in Options menu
+			aurasHeader = {
+				order = 10,
+				type = 'header',
+				name = "Auras"
+			},
+			auraDescription = {
+				order = 11,
 				type = 'description',
 				name = "Options for spells that adds an aura to your character.",
 			},
-			spacer = {
-				order = 1,
-				type = 'header',
-				name = ""
-			},
-			enable = {
-				order = 2,
+			auraAllEnable = {
+				order = 12,
 				type = 'toggle',
 				name = 'Announce auras',
 				desc = 'Enable or disable all announcements connected to an Aura',
@@ -36,28 +52,29 @@ function SAC:CreateOptions()
 				get = 'Get',
 			},
 			auras = {
-				order = 10,
+				order = 13,
 				type = 'select',
 				name = 'Auras',
 				values = SAC.namedAuraList,
 				style = 'radio',
-				set = 'SetAuraOptions',
-				get = 'GetAuraOptions',
+				set = 'SetAuraList',
+				get = 'GetAuraList',
 			},
 			auraSettings = {
+				order = 14,
 				name = "",
 				type = 'group',
 				guiInline = true,
 				args = {
 					announceStart = {
-						order = 11,
+						order = 0,
 						type = 'toggle',
 						name = "Announce start",
 						set = 'SetAuraToggle',
 						get = 'GetAuraToggle',
 					},
 					announceEnd = {
-						order = 12,
+						order = 1,
 						type = 'toggle',
 						name = "Announce end",
 						set = 'SetAuraToggle',
@@ -65,26 +82,26 @@ function SAC:CreateOptions()
 					},
 				},
 			},
-		},
-	}
-	
-	SAC.Options_Resists = {
-		name = "Resists",
-		handler = SAC,
-		type = 'group',
-		args = {
-			description = {
-				order = 0,
+			
+			spacer1 = {
+				order = 29,
+				type = 'description',
+				name = " ",
+			},
+			
+			-- Resists section in the Options menu
+			resistsHeader = {
+				order = 30,
+				type = 'header',
+				name = "Resits",
+			},
+			resistsDescription = {
+				order = 31,
 				type = 'description',
 				name = "Options for spells that fail when casted on a target. This includes all forms of resists.",
 			},
-			spacer = {
-				order = 1,
-				type = 'header',
-				name = "",
-			},
-			enable = {
-				order = 2,
+			resistsAllEnable = {
+				order = 32,
 				type = 'toggle',
 				name = 'Announce failed spells',
 				desc = 'Enable or disable all announcements connected to an Aura',
@@ -92,110 +109,201 @@ function SAC:CreateOptions()
 				get = 'Get',
 			},
 			spells = {
-				order = 10,
+				order = 40,
 				type = 'select',
 				name = 'Spells',
 				values = SAC.namedResistList,
 				style = 'radio',
-				set = 'SetResistOptions',
-				get = 'Get',
+				set = 'SetResistList',
+				get = 'GetResistList',
+			},
+			resistSettings = {
+				order = 41,
+				name = "",
+				type = 'group',
+				guiInline = true,
+				args = {
+					resistAnnounceEnabled = {
+						order = 0,
+						type = 'toggle',
+						name = "Announce Enabled",
+						set = 'SetResistToggle',
+						get = 'GetResistToggle',
+					},
+				},
 			},
 		},
 	}
+
 	
-	self:SetDefaultOptions()
+	self:InitializeOptions()
 	
 	config:RegisterOptionsTable("SAC_Options", SAC.Options)
 	self.optionsFrame = dialog:AddToBlizOptions("SAC_Options", SAC.Options.name)
-	config:RegisterOptionsTable("SAC_Options_Auras", SAC.Options_Auras)
-	self.optionsAurasFrame = dialog:AddToBlizOptions("SAC_Options_Auras", SAC.Options_Auras.name, SAC.Options.name)
-	config:RegisterOptionsTable("SAC_Options_Resists", SAC.Options_Resists)
-	self.optionsResistsFrame = dialog:AddToBlizOptions("SAC_Options_Resists", SAC.Options_Resists.name, SAC.Options.name)
-
+	
+	
 end
 
 
-function SAC:SetDefaultOptions()
-	
+function SAC:InitializeOptions()
+
+
 	if self.db.char.options == nil then
 		self.db.char.options = {}
-		for k,v in pairs(self.namedAuraList) do
-			table.insert(self.db.char.options, v)
-			self.db.char.options[v] = {}
-		end
+	end
+
+	for k,v in pairs(self.namedAuraList) do
+		self.db.char.options[v] = {}
+	end
+	for k,v in pairs(self.namedResistList) do
+		self.db.char.options[v] = {}
 	end
 	
 end
 
 
-function SAC:SetAuraOptions(info, val)
+function SAC:SetAuraList(info, val)
+
 	self.db.char.options[info[#info]] = val
 	self.lastSelectedAura = info["option"]["values"][val]
-	self.Options_Auras.args.auraSettings.name = self.lastSelectedAura
+	self.Options.args.auraSettings.name = self.lastSelectedAura
+	
 end
 
 
-function SAC:GetAuraOptions(info)
+function SAC:GetAuraList(info)
+
+	-- Default Settings
+	-- Select the first spell in the Aura list.
 	if self.db.char.options[info[#info]] == nil then
-		self.db.char.options[info[#info]] = true
+		self.db.char.options[info[#info]] = 1
 	end
 	
-	if self.Options_Auras.args.auraSettings.name == "" then
-		self.Options_Auras.args.auraSettings.name = self.namedAuraList[1]
+	--print(self.db.char.options[info[#info]], info, info[#info])
+	
+	-- Do the same for the Aura settings.
+	if self.Options.args.auraSettings.name == "" then
+		self.Options.args.auraSettings.name = self.namedAuraList[self.db.char.options[info[#info]]] or self.namedAuraList[1]
+	end
+	
+	return self.db.char.options[info[#info]]
+	
+end
+
+function SAC:SetResistList(info, val)
+
+	self.db.char.options[info[#info]] = val
+	self.lastSelectedResist = info["option"]["values"][val]
+	self.Options.args.resistSettings.name = self.lastSelectedResist
+	
+end
+
+
+function SAC:GetResistList(info)
+	
+	-- Default Settings
+	-- Select the first spell in the Resist list.
+	if self.db.char.options[info[#info]] == nil then
+		self.db.char.options[info[#info]] = 1
+	end
+	
+	-- Do the same for the Resist settings.
+	if self.Options.args.resistSettings.name == "" then
+		self.Options.args.resistSettings.name = self.namedResistList[self.db.char.options[info[#info]]] or self.namedResistList[1]
 	end
 	
 	return self.db.char.options[info[#info]]
 end
 
 
-function SAC:SetResistOptions(info, val)
-	self.db.char.options[info[#info]] = val
-	self.lastSelectedResist = info["option"]["values"][val]
-end
-
-
 function SAC:SetAuraToggle(info, val)
+
 	self.db.char.options[self.lastSelectedAura][info[#info]] = val
-end
-
-
-function SAC:SetResistToggle(info, val)
-	self.db.char.options[self.lastSelectedResist][info[#info]] = val
-end
-
-
-function SAC:Set(info, val)
-	self.db.char.options[info[#info]] = val
+	
 end
 
 
 function SAC:GetAuraToggle(info)
+
+	-- Select first Aura in the Aura list if no aura has been selected before.
 	if self.lastSelectedAura == nil then
 		self.lastSelectedAura = self.namedAuraList[1]
 	end
 	
+	-- Default settings
+	if self.db.char.options[self.lastSelectedAura][info[#info]] == nil then
+		if info[#info] == "announceStart" then
+			self.db.char.options[self.lastSelectedAura][info[#info]] = true
+		elseif info[#info] == "announceEnd" then
+			self.db.char.options[self.lastSelectedAura][info[#info]] = false
+		end
+	end
+	
 	return self.db.char.options[self.lastSelectedAura][info[#info]]
+	
+end
+
+
+function SAC:SetResistToggle(info, val)
+
+	--print(self.lastSelectedResist, info[#info], val)
+	self.db.char.options[self.lastSelectedResist][info[#info]] = val
+	
 end
 
 
 function SAC:GetResistToggle(info)
+
+	-- Select first Spell in the Resist list if no spell has been selected before.
 	if self.lastSelectedResist == nil then
-		self.lastSelectedResist = self.namedAuraList[1]
+		self.lastSelectedResist = self.namedResistList[1]
 	end
 	
+	-- Default settings
+	if self.db.char.options[self.lastSelectedResist][info[#info]] == nil then
+		self.db.char.options[self.lastSelectedResist][info[#info]] = true
+	end
+
 	return self.db.char.options[self.lastSelectedResist][info[#info]]
+	
+end
+
+
+function SAC:SetChatChannel(info, val)
+
+	self.db.char.options[info[#info]] = val
+	
+end
+
+
+function SAC:GetChatChannel(info)
+
+
+	-- Default settings
+	if self.db.char.options[info[#info]] == nil then
+		self.db.char.options[info[#info]] = "YELL"
+	end
+		
+	return self.db.char.options[info[#info]]
+	
+end
+
+
+
+function SAC:Set(info, val)
+
+	self.db.char.options[info[#info]] = val
+	
 end
 
 
 function SAC:Get(info)
+
+	-- Default settings
 	if self.db.char.options[info[#info]] == nil then
 		self.db.char.options[info[#info]] = true
 	end
 		
 	return self.db.char.options[info[#info]]
-end
-
-
-function InterfaceOptionsFrame_OnEvent (self, event, ...)
-	print(event)
+	
 end
